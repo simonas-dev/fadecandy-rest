2876aa4118697a8098247e173207ef75db344375
Simonas Sankauskas <simonas.sank@gmail.com>
2019-04-23 21:55:11 +0300

    Proof Of Concept

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,26 @@
 package dev.simonas
 
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
 import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+  install(Routing) {
+        get("/") {
+            val process = Runtime.getRuntime().exec("pwd")
+            process.waitFor()
+            val outByteArr = process.inputStream.readBytes()
+            val outString = String(outByteArr)
+            call.respondText("pwd: $outString", ContentType.Text.Html)
+        }
+    }
 }
 
